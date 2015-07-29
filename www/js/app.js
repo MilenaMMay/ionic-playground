4055c9fc461b71bfb2004881907c44e11726ee81
Milena May <MilenaMMay@googlemail.com>
2015-07-29 10:59:39 +0200

    Verschiebe den generierten Javascript Code von der index.html in app.js um Komponenten und Verhalten zu trennen

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,60 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// Den Namen, den Du Deinem Modul hier im Hauptprogramm vergibst ('app'), musst Du auch in der index.html nutzen, um die ng-app zu benennen.
+// So verknüpfst Du das HTML (die Komponenten Deiner App) mit dem Javascript (dem Verhalten Deiner App).
+// Die Liste, die danach übergeben wird, sind weitere Javascript-Module, die Deine App benötigt ("required dependencies", benötigte Abhängigkeiten).
+// * 'ionic' benötigst Du, weil wir dieses Framework (Rahmen) verwenden.
+angular.module('app', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
     }
-    if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    .state('form', {
+      url: '/form',
+      templateUrl: 'templates/form.html'
+    })
+
+    .state('list', {
+      url: '/list',
+      templateUrl: 'templates/list.html'
+    })
+
+    .state('search', {
+      url: '/search',
+      templateUrl: 'templates/search.html'
+    })
+
+    .state('hello', {
+      url: '/hello',
+      templateUrl: 'templates/hello.html'
+    })
+
+    .state('text', {
+      url: '/text',
+      templateUrl: 'templates/text.html'
+    })
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+
+  $urlRouterProvider.otherwise('/form');
+
+
 });
